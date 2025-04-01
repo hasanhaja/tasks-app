eca92bdac235561bf8692931f03d12878eb16b6c
Hasan Ali <hasanhaja@gmail.com>
2025-04-02 00:22:43 +0100

    feat: tweak styling and add filter scaffolding

--- _site/sw.js
+++ _site/sw.js
@@ -114,7 +114,18 @@ function list(id, title, completed) {
         >
         ${ completed ?
             `<span><s>${title}</s></span>` 
-          : `<a href="/edit?id=${id}"><span class="sr-only">Edit</span><span>${title}</span></a>`
+          : `
+            <span>
+              <span>${title}</span>
+              <a href="/edit?id=${id}">
+                <span class="sr-only">Edit</span>
+                <span class="edit-task">
+                  <!-- TODO Replace with font awesome icon -->
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg>
+                </span>
+              </a>
+            </span>
+          `
         }
       </label>
     </li>
